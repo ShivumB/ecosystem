6f4c22eecd4101bc91a143c4767dd1d5977ca0b0
Shivum Banerjee <73792393+ShivumB@users.noreply.github.com>
2023-05-29 17:12:45 +0000

    Created bunny decision making function

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var bunnies;
+var foxes;
 var carrots;
 var water;
 
@@ -7,155 +8,266 @@ var frame = 0;
 var iteration = 0;
 
 function Bunny(x, y) {
-  
+
+  this.r = 15;
+
   this.x = x;
   this.y = y;
-  
+
+  this.velX = 0;
+  this.velY = 0;
+
+  this.accelX = 0;
+  this.accelY = 0;
+
+  this.speed = 1;
+  this.vision = 100;
+
   this.hunger = 0;
   this.selectedFood = -1;
-  
+
   this.thirst = 0;
   this.selectedWater = -1;
-  
-  this.theta = 0;
-  
+
   this.urge = 0;
-  
-  this.vision = 100;
-  
+
+  this.behavior = -1;
+
   this.frame = 0;
 
 }
 
-Bunny.prototype.findFood = function(carrots) {
-  
+Bunny.prototype.findFood = function (carrots) {
+
   this.selectedFood = -1;
   let minDist = 1000000;
-  
-  for(let i = 0; i < carrots.length; i++) {
-    
-    if(carrots[i].alive && Math.abs(carrots[i].x - this.x) < this.vision && Math.abs(carrots[i].y - this.y) < this.vision) { 
-      
-      let distX = carrots[i].x - this.x;
-      let distY = carrots[i].y - this.y;
-      
-      if(distX * distX + distY * distY < minDist) {
+
+  for (let i = 0; i < carrots.length; i++) {
+
+    let distX = carrots[i].x - this.x;
+    let distY = carrots[i].y - this.y;
+
+    if (carrots[i].alive && distX * distX + distY * distY < Math.pow(this.vision + carrots[i].r, 2)) {
+
+      if (distX * distX + distY * distY < minDist) {
         minDist = distX * distX + distY * distY;
         this.selectedFood = i;
-      }  
+      }
     }
-    
+
   }
-  
-  if(this.selectedFood >= 0) {
-    
+
+  let distX = 0;
+  let distY = 0;
+  if (this.selectedFood >= 0) {
+    distX = carrots[this.selectedFood].x - this.x;
+    distY = carrots[this.selectedFood].y - this.y;
+  }
+
+  if (this.selectedFood >= 0 && distX * distX + distY * distY < Math.pow(carrots[this.selectedFood].r + this.r, 2)) {
+    carrots[this.selectedFood].alive = false;
+    this.hunger = Math.max(0, this.hunger - 5);
+
+    if (this.hunger < 5) this.behavior = -1;
+
+  } else if (this.selectedFood >= 0) {
+
     let xDist = (carrots[this.selectedFood].x - this.x);
     let yDist = (carrots[this.selectedFood].y - this.y);
-    let mag = Math.sqrt(xDist*xDist + yDist*yDist);
-    
+    let mag = Math.sqrt(xDist * xDist + yDist * yDist);
+
     this.x += 1 * (carrots[this.selectedFood].x - this.x) / mag;
     this.y += 1 * (carrots[this.selectedFood].y - this.y) / mag;
-    
-  }
-  
-  if(this.selectedFood >= 0 && Math.abs(carrots[this.selectedFood].x - this.x) < 30 && Math.abs(carrots[this.selectedFood].y - this.y) < 30) {
-    carrots[this.selectedFood].alive = false;
-    this.hunger -= 100;
+
+  } else {
+    this.explore();
   }
 }
 
-Bunny.prototype.findWater = function(water) {
-  
+Bunny.prototype.findWater = function (water) {
+
   this.selectedWater = -1;
   let minDist = 1000000;
-  
-  for(let i = 0; i < water.length; i++) {
-    
-    if(Math.abs(water[i].x - this.x) < this.vision && Math.abs(water[i].y - this.y) < this.vision) { 
-      
-      let distX = water[i].x - this.x;
-      let distY = water[i].y - this.y;
-      
-      if(distX * distX + distY * distY < minDist) {
+
+  for (let i = 0; i < water.length; i++) {
+
+    let distX = water[i].x - this.x;
+    let distY = water[i].y - this.y;
+
+    if (distX * distX + distY * distY < Math.pow(this.vision + water[i].r, 2)) {
+
+
+
+      if (distX * distX + distY * distY < minDist) {
         minDist = distX * distX + distY * distY;
         this.selectedWater = i;
-      }  
+      }
     }
-    
+
   }
-  
-  
-  if(this.selectedWater >= 0 && Math.abs(water[this.selectedWater].x - this.x) < 15 + water[this.selectedWater].r && Math.abs(water[this.selectedWater].y - this.y) < 15 + water[this.selectedWater].r) {
-    
-    this.thirst -= 1;
-    
-  } else if(this.selectedWater >= 0) {
-    
+
+  let distX = 0;
+  let distY = 0;
+  if (this.selectedWater >= 0) {
+    distX = water[this.selectedWater].x - this.x;
+    distY = water[this.selectedWater].y - this.y;
+  }
+
+  if (this.selectedWater >= 0 && distX * distX + distY * distY < Math.pow(water[this.selectedWater].r + this.r + 5, 2)) {
+
+    this.thirst = Math.max(0, this.thirst - 5);
+
+    if (this.thirst < 10) this.behavior = -1;
+
+  } else if (this.selectedWater >= 0) {
+
     let xDist = (water[this.selectedWater].x - this.x);
     let yDist = (water[this.selectedWater].y - this.y);
-    let mag = Math.sqrt(xDist*xDist + yDist*yDist);
-    
+    let mag = Math.sqrt(xDist * xDist + yDist * yDist);
+
     this.x += 1 * (water[this.selectedWater].x - this.x) / mag;
     this.y += 1 * (water[this.selectedWater].y - this.y) / mag;
-    
+
+  } else {
+    this.explore();
   }
-  
 }
 
-Bunny.prototype.reproduce = function(bunnies) {
-  
-}
+Bunny.prototype.explore = function () {
 
-Bunny.prototype.act = function(carrots, water) {
-  
-  console.log(this.hunger + ", " + this.thirst);
-  
-  if(this.hunger >= this.thirst) {
-    this.findFood(carrots);
-    
-  } else if(this.thirst > this.hunger) {
-    this.findWater(water);
+  if (this.frame == 10) {
+
+    this.accelX = Math.random() * 1 - 0.5;
+    this.accelY = Math.random() * 1 - 0.5;
+
   }
-  
-  
-  if(this.foundFood == -1 && this.selectedWater == -1) {
-    
-    console.log("what!");
-    
-    if(this.frame == 10) {
-      this.theta += Math.random()*360;
-    } else if(this.frame > 10) {
-      this.x += Math.cos(this.theta);
-      this.y += Math.cos(this.theta);
-    }
-    
-  }
-  
-  this.reproduce(bunnies);
-  
-  
-  fill(255, 255, 0);
-  ellipse(this.x, this.y, 30, 30);
-  
-  this.hunger += 2;
-  this.thirst += 0.5;
-  
+
   this.frame++;
   this.frame %= 20;
-  
+
+  this.behavior = -1;
+
+}
+
+Bunny.prototype.reproduce = function (bunnies) {
+
+}
+
+Bunny.prototype.flee = function (foxes) {
+
+}
+
+Bunny.prototype.decideBehavior = function (foxes) {
+
+  // 0 - find water, 1 - find food, 2 - reproduce, 3 - flee
+
+  //not doing anything
+  if (this.behavior == -1) {
+
+    //first, check water
+    if (this.thirst > 20) {
+      this.behavior = 0;
+    } else if (this.hunger > 10) {
+      this.behavior = 1;
+    } else if (this.urge > 10) {
+      this.behavior = 2;
+    }
+
+  } else {
+    //finish this behavior, then move on - set behavior to -1 in behavior methods to indicate its over
+    //unless being hunted - then go into flight
+
+    for (let i = 0; i < foxes.length; i++) {
+      let distX = foxes[i].x - this.x;
+      let distY = foxes[i].y - this.y;
+
+      if (distX * distX + distY * distY < this.vision * this.vision) {
+        this.behavior = 4;
+      }
+    }
+
+
+  }
+
+}
+
+Bunny.prototype.act = function (bunnies, foxes, carrots, water) {
+
+
+  this.decideBehavior(foxes);
+
+  if (this.behavior == 0) {
+    this.findWater(water);
+  } else if (this.behavior == 1) {
+    this.findFood(carrots);
+  } else if (this.behavior == 2) {
+    this.reproduce(bunnies);
+  } else if (this.behavior == 3) {
+    this.flee(foxes);
+  }
+
+
+  //DISPLAY BUNNY
+  textSize(20);
+  fill(255, 100);
+  rect(this.x, this.y - 20, 40, 20);
+  fill(0);
+  text("hunger: " + Math.floor(this.hunger) + ", thirst:" + Math.floor(this.thirst) + ", found food: " + this.selectedFood + ", found water: " + this.selectedWater + ", behavior: " + this.behavior, this.x, this.y - 20);
+
+  fill(255, 255, 0);
+  ellipse(this.x, this.y, 30, 30);
+
+  //RUN COLLISIONS
+  for (let i = 0; i < water.length; i++) {
+
+    let distX = this.x - water[i].x;
+    let distY = this.y - water[i].y;
+
+    let mag = distX * distX + distY * distY;
+    let sqrtMag = Math.sqrt(mag);
+
+    if (distX * distX + distY * distY < (water[i].r + this.r) * (water[i].r + this.r)) {
+
+      this.x = water[i].x + distX / sqrtMag * (water[i].r + this.r);
+      this.y = water[i].y + distY / sqrtMag * (water[i].r + this.r);
+
+    }
+  }
+
+  //UPDATE POSITION
+  this.velX += this.accelX;
+  this.velY += this.accelY;
+
+  if (this.velY < -this.speed) this.velY = -this.speed;
+  if (this.velY > this.speed) this.velY = this.speed;
+  if (this.velX < -this.speed) this.velX = -this.speed;
+  if (this.velX > this.speed) this.velX = this.speed;
+
+  this.x += this.velX;
+  this.y += this.velY;
+
+  if (this.x < 0) this.x = 0;
+  if (this.x > width) this.x = width;
+  if (this.y < 0) this.y = 0;
+  if (this.y > height) this.y = height;
+
+  this.hunger += 0.01;
+  this.thirst += 0.05;
 }
 
 function Carrot(x, y) {
-  
+
   this.x = x;
   this.y = y;
-  
+
+  this.r = 15;
+
   this.alive = true;
 }
 
-Carrot.prototype.act = function() {
-  
-  if(this.alive) {
+Carrot.prototype.act = function () {
+
+  if (this.alive) {
     fill(0, 9, 0);
     ellipse(this.x, this.y, 30, 30);
   }
@@ -165,54 +277,72 @@ Carrot.prototype.act = function() {
 function Water(x, y) {
   this.x = x;
   this.y = y;
-  
+
   this.r = 150;
 }
 
-Water.prototype.act = function() {
-  
+Water.prototype.act = function () {
   fill(0, 255, 255);
-  ellipse(this.x, this.x, 2*this.r, 2*this.r);
-  
+  ellipse(this.x, this.x, 2 * this.r, 2 * this.r);
 }
 
 function setup() {
 
   angleMode(DEGREES);
-  
-  carrots = [];
-  for(let i = 0; i < 100; i++) {
-    carrots.push(new Carrot(Math.random()*1180 + 10, Math.random()*580 + 10));
-  }
-  
+
   water = [];
   water.push(new Water(300, 300));
-  water.push(new Water(900, 300));
+  //water.push(new Water(900, 300));
+
+  carrots = [];
+  for (let i = 0; i < 100; i++) {
+    carrots.push(new Carrot(Math.random() * 1180 + 10, Math.random() * 580 + 10));
+
+    let cool = false;
+
+    while (!cool) {
+
+      cool = true;
+      for (let j = 0; j < water.length; j++) {
+        let distX = carrots[i].x - water[j].x;
+        let distY = carrots[i].y - water[j].y;
+
+        if (distX * distX + distY * distY < Math.pow(carrots[i].r + water[j].r, 2)) {
+          this.x = Math.random() * 1180 + 10;
+          this.y = Math.random() * 580 + 10;
+          cool = false;
+        }
+      }
+
+    }
+  }
 
   bunnies = [];
-  for(let i = 0; i < 10; i++) {
-    bunnies.push(new Bunny(Math.random()*1180 + 10, Math.random()*580 + 10));
+  for (let i = 0; i < 2; i++) {
+    bunnies.push(new Bunny(Math.random() * 1180 + 10, Math.random() * 580 + 10));
   }
-  
+
+  foxes = [];
+
   createCanvas(1200, 600);
-  
+
 }
 
 function draw() {
-    
+
   background(100, 255, 100);
-    
-  for(let i = 0; i < carrots.length; i++) {
+
+  for (let i = 0; i < carrots.length; i++) {
     carrots[i].act();
   }
-  
-  for(let i = 0; i < water.length; i++) {
+
+  for (let i = 0; i < water.length; i++) {
     water[i].act();
   }
 
-  for(let i = 0; i < bunnies.length; i++) {
-    bunnies[i].act(carrots, water);
+  for (let i = 0; i < bunnies.length; i++) {
+    bunnies[i].act(bunnies, foxes, carrots, water);
   }
-  
-  
+
+
 }
